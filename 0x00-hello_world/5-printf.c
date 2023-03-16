930269d73c625e9de44023a6acace7b7fc8063ac
Emmanuel Akintunde Shotolu <nerad9@gmail.com>
2023-03-16 10:05:49 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Description: This program prints a string with printf
+ *
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
