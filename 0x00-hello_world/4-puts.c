14b54853b6ba441600d0a83ddbb4ba7ae25e81f8
Emmanuel Akintunde Shotolu <nerad9@gmail.com>
2023-03-16 09:52:19 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Description: This program prints the string to the console.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
